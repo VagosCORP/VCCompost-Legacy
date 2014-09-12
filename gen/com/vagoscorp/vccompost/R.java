cf53f909a2faa0ccbefcd9dbc5035531c67b3d00
Sebasu <Sebasu_Knutti@hotmail.com>
2014-09-12 00:00:04 -0400

    Communication Update

--- gen/com/vagoscorp/vccompost/R.java
+++ gen/com/vagoscorp/vccompost/R.java
@@ -24,8 +24,8 @@ public final class R {
          would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
     
          */
-        public static final int activity_horizontal_margin=0x7f070000;
-        public static final int activity_vertical_margin=0x7f070001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
@@ -85,43 +85,43 @@ public final class R {
         public static final int save_rfiles=0x7f080003;
     }
     public static final class string {
-        public static final int A_Dat=0x7f050011;
-        public static final int C_Server=0x7f050012;
-        public static final int Ejecutar=0x7f05001a;
-        public static final int Erase_Data=0x7f050014;
-        public static final int EspejoAB=0x7f05001d;
-        public static final int FileNames=0x7f050023;
-        public static final int FileNamesH=0x7f050024;
-        public static final int FileSA=0x7f050020;
-        public static final int FileSB=0x7f050021;
-        public static final int GuardarRutina=0x7f05001e;
-        public static final int Hora=0x7f050005;
-        public static final int Load_Data=0x7f050013;
-        public static final int OK=0x7f05000c;
-        public static final int PuntoIn=0x7f05001b;
-        public static final int PuntoOut=0x7f05001c;
-        public static final int Reset_Comunication=0x7f050015;
-        public static final int RnameH=0x7f050022;
-        public static final int SeccionA=0x7f050018;
-        public static final int SeccionB=0x7f050019;
-        public static final int Ser_Dat=0x7f050010;
-        public static final int Server_IP=0x7f05000a;
-        public static final int Server_Port=0x7f05000b;
-        public static final int Temp1=0x7f050006;
-        public static final int Temp2=0x7f050007;
-        public static final int Temp3=0x7f050008;
-        public static final int Temp4=0x7f050009;
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int gru_donde=0x7f05000d;
-        public static final int gru_hora=0x7f05000f;
-        public static final int gru_llego=0x7f05000e;
-        public static final int hello_world=0x7f050002;
-        public static final int routine=0x7f050017;
-        public static final int set_server=0x7f050003;
-        public static final int title_activity_dat_vag=0x7f050004;
-        public static final int title_activity_routine=0x7f050016;
-        public static final int title_activity_save_rfiles=0x7f05001f;
+        public static final int A_Dat=0x7f060011;
+        public static final int C_Server=0x7f060012;
+        public static final int Ejecutar=0x7f06001a;
+        public static final int Erase_Data=0x7f060014;
+        public static final int EspejoAB=0x7f06001d;
+        public static final int FileNames=0x7f060023;
+        public static final int FileNamesH=0x7f060024;
+        public static final int FileSA=0x7f060020;
+        public static final int FileSB=0x7f060021;
+        public static final int GuardarRutina=0x7f06001e;
+        public static final int Hora=0x7f060005;
+        public static final int Load_Data=0x7f060013;
+        public static final int OK=0x7f06000c;
+        public static final int PuntoIn=0x7f06001b;
+        public static final int PuntoOut=0x7f06001c;
+        public static final int Reset_Comunication=0x7f060015;
+        public static final int RnameH=0x7f060022;
+        public static final int SeccionA=0x7f060018;
+        public static final int SeccionB=0x7f060019;
+        public static final int Ser_Dat=0x7f060010;
+        public static final int Server_IP=0x7f06000a;
+        public static final int Server_Port=0x7f06000b;
+        public static final int Temp1=0x7f060006;
+        public static final int Temp2=0x7f060007;
+        public static final int Temp3=0x7f060008;
+        public static final int Temp4=0x7f060009;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int gru_donde=0x7f06000d;
+        public static final int gru_hora=0x7f06000f;
+        public static final int gru_llego=0x7f06000e;
+        public static final int hello_world=0x7f060002;
+        public static final int routine=0x7f060017;
+        public static final int set_server=0x7f060003;
+        public static final int title_activity_dat_vag=0x7f060004;
+        public static final int title_activity_routine=0x7f060016;
+        public static final int title_activity_save_rfiles=0x7f06001f;
     }
     public static final class style {
         /** 
@@ -144,41 +144,19 @@ public final class R {
         res/values-v11/styles.xml on API 14+ devices.
     
  API 14 theme customizations can go here. 
-
-        Base application theme, dependent on API level. This theme is replaced
-        by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
-    
-
-            Theme customizations available in newer API levels can go in
-            res/values-vXX/styles.xml, while customizations related to
-            backward-compatibility can go here.
-        
-
-        Base application theme for API 11+. This theme completely replaces
-        AppBaseTheme from res/values/styles.xml on API 11+ devices.
-    
- API 11 theme customizations can go here. 
-
-        Base application theme for API 14+. This theme completely replaces
-        AppBaseTheme from BOTH res/values/styles.xml and
-        res/values-v11/styles.xml on API 14+ devices.
-    
- API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
- Application theme. 
- All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
         /**  All customizations that are NOT specific to a particular API-level can go here. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int Dialog=0x7f060002;
+        public static final int Dialog=0x7f070002;
         /**  All customizations that are NOT specific to a particular API-level can go here. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int Dialogl=0x7f060003;
+        public static final int Dialogl=0x7f070003;
     }
 }
